711c46bd4bc88abb8f51439c2a7d7dcc9f590d52
Martin Thiim <martin@thiim.net>
2020-12-05 18:02:13 +0000

    Update

--- index.js
+++ index.js
@@ -331,7 +331,7 @@ async function update() {
             ],
             labels: labels
         };
-        insertChart(region.name + " (infected per day as 7-day moving average)", data, "Infected per day (7-day moving average)", "Rt");
+        insertChart(region.name + " (infected per day as 7-day moving average, and estimated reproduction number)", data, "Infected per day (7-day moving average)", "Estimated reproduction number");
         i++;
     }
 }
